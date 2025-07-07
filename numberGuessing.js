58a14833b7ca51096a9b90391959d93bae5a4ad5
raffarizzy <raffarizkyf@gmail.com>
2025-07-07 15:48:15 +0700

    added winning state check and js done

--- numberGuessing.js
+++ numberGuessing.js
@@ -2,17 +2,23 @@ let choosenNumber = Math.floor(Math.random() * 101) + 1;
 let inputField = document.getElementById("inputField");
 let guessBtn = document.getElementById("guessBtn");
 let result = document.getElementById("result");
+const winSound = document.getElementById("winSound");
+let gameStateWin = false;
 let inputedNumber = 0;
+
 
 inputField.addEventListener("input", function() {
     this.value = this.value.replace(/\D/g, "");
 })
 
 guessBtn.onclick = function() {
-    inputedNumber = Number(inputField.value);
+    if (gameStateWin) return;
     
+    inputedNumber = Number(inputField.value);
     if (inputedNumber == choosenNumber) {
-        result.textContent = "CORRECT!";
+        result.innerHTML = "CORRECT!" + "<br>" + "REFRESH TO TRY AGAIN!";
+        winSound.play();
+        gameStateWin = true;
     }
 
     if(inputedNumber > choosenNumber) {
